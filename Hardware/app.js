8ac7f856605e8adff1dec94701df78b70319344a
Alex <alex.frank4568@gmail.com>
2018-12-10 18:20:49 +0300

    Made navigation

--- Hardware/app.js
+++ Hardware/app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs', require('ejs-locals'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/bootstrap", express.static(__dirname + "node_modules/bootstrap/dis"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Hardware/routes/index.js
+++ Hardware/routes/index.js
@@ -10,32 +10,32 @@ router.get('/', function(req, res, next) {
 
 /* Motherboard  */
 router.get('/motherboard', function(req, res, next) {
-  res.render ('hardviews', {title: 'Материнская плата', picture: 'images/motherboard.jpg', desc: 'Motherboard'})
+  res.render ('hardviews', {title: 'Материнская плата', picture: 'images/motherboard.jpg', desc: 'Материнская плата'})
 });
 
 /* CPU  */
 router.get('/CPU', function(req, res, next) {
-  res.render ('hardviews', {title: 'Процессор', picture: 'images/CPU.jpg', desc: 'CPU'})
+  res.render ('hardviews', {title: 'Процессор', picture: 'images/CPU.jpg', desc: 'Процессор'})
 });
 
 /* Video card  */
 router.get('/videocard', function(req, res, next) {
-  res.render ('hardviews', {title: 'Видеокарта', picture: 'images/Videocard.jpg', desc: 'Video card'})
+  res.render ('hardviews', {title: 'Видеокарта', picture: 'images/Videocard.jpg', desc: 'Видеокарта'})
 });
 
 /* Power supply  */
 router.get('/powersupply', function(req, res, next) {
-  res.render ('hardviews', {title: 'Блок питания', picture: 'images/Powersupply.jpg', desc: 'Power supply'})
+  res.render ('hardviews', {title: 'Блок питания', picture: 'images/Powersupply.jpg', desc: 'Блок питания'})
 });
 
 /* HDD  */
 router.get('/HDD', function(req, res, next) {
-  res.render ('hardviews', {title: 'HDD', picture: 'images/HDD.jpg', desc: 'HDD'})
+  res.render ('hardviews', {title: 'HDD', picture: 'images/HDD.jpg', desc: 'Жёсткий диск'})
 });
 
 /* RAM  */
 router.get('/RAM', function(req, res, next) {
-  res.render ('hardviews', {title: 'ОЗУ', picture: 'images/RAM.jpg', desc: 'RAM'})
+  res.render ('hardviews', {title: 'ОЗУ', picture: 'images/RAM.jpg', desc: 'Оперативная память'})
 });
 
 
